c09fb3114334fedf2ffc8dc8e6053f78763855b3
Luke Miles <rashreportloaf@gmail.com>
2016-08-04 11:26:31 -0400

    changed init for Profile to not require mayors arg

--- social-choice-functions.py
+++ social-choice-functions.py
@@ -3,9 +3,10 @@ import random
 class Profile(object):
     # it would save computation if I made graphs once at the start
     # todo: combine pairs that have same ballot instead of destroying one
-    def __init__(self, mayors, pairs):
-        self.mayors = mayors
+    def __init__(self, pairs):
         self.pairs = pairs
+        self.mayors = set(next(iter(pairs))[1])
+        self.totalVotes = sum(numVotes for (numVotes, _) in pairs)
 
     def netPreference(self, mayor1, mayor2):
         return sum(numVotes
@@ -24,7 +25,11 @@ class Profile(object):
         return sum(numVotes * (topScore - ballot.index(mayor))
                    for numVotes, ballot in self.pairs)
 
-    ## condorcet consistent scores
+    ## condorcet consistent scores and SCF's
+    def condorcetWinners(self):
+        return {mayor for mayor in self.mayors
+                if all(self.netPreference(mayor, mayor2)>=0
+                       for mayor2 in self.mayors)}
     def simpsonScore(self, mayor):
         return min(self.netPreference(mayor, mayor2)
                    for mayor2 in self.mayors - {mayor})
@@ -48,36 +53,17 @@ def sign(n):
 def removeIndex(aTuple, index):
     return aTuple[:index] + aTuple[index+1:]
 
-profile1 = Profile({0,1,2},{(102,(0,1,2)), (101,(1,2,0)), (100,(2,0,1)), (1,(2,1,0))})
-profile2 = Profile({0,1,2}, {(101,(2,1,0)), (100,(0,2,1))})
-profile3 = Profile({0,1,2},((40,(0,1,2)),(28,(1,2,0)),(32,(2,1,0))))
-profile4 = Profile({0,1,2,3,4},{(15,(1,2,4,0,3)),(29,(0,1,3,4,2)),(42,(2,1,3,0,4)),(43,(4,1,3,2,0)),(45,(1,2,3,0,4)),(52,(3,0,1,2,4)),(53,(0,2,1,3,4)),(59,(1,2,3,4,0)),(60,(1,4,3,0,2)),(87,(1,4,0,2,3))})
+profile1=Profile({(102,(0,1,2)),(101,(1,2,0)),(100,(2,0,1)),(1,(2,1,0))})
+profile2=Profile({(101,(2,1,0)),(100,(0,2,1))})
+profile3=Profile({(40,(0,1,2)),(28,(1,2,0)),(32,(2,1,0))})
+profile4=Profile({(15,(1,2,4,0,3)),(29,(0,1,3,4,2)),(42,(2,1,3,0,4)),(43,(4,1,3,2,0)),(45,(1,2,3,0,4)),(52,(3,0,1,2,4)),(53,(0,2,1,3,4)),(59,(1,2,3,4,0)),(60,(1,4,3,0,2)),(87,(1,4,0,2,3))})
+profile5=Profile({(20,(0,1,2)),(20,(1,2,0)),(20,(2,0,1))})
 
-### CONDORCET EXTENSIONS
-#
-#
-#def simpsonSCF(profile):
-#    _, ballot = next(iter(profile))
-#    return max(ballot, key=lambda candidate: simpsonScore(profile, candidate))
-#
-#
-### OTHER STUFF
-#
 #def doesParetoDominate(profile, candidate1, candidate2):
 #    # assumes all pairs in profile have at least one voter
 #    # todo: test if this works
 #    return all(ballot.index(candidate1) < ballot.index(candidate2)
 #               for _, ballot in profile)
-#
-#def condorcetWinner(profile):
-#    _, ballot = next(iter(profile))
-#    for candidate1 in ballot:
-#      if any(netPreference(profile, candidate2, candidate1) > 0
-#             for candidate2 in ballot):
-#          next
-#      else:
-#          return candidate1
-#    return False
 #
 #def singleTransferableVote(profile):
 #    # messy. I think it works.
@@ -94,5 +80,3 @@ profile4 = Profile({0,1,2,3,4},{(15,(1,2,4,0,3)),(29,(0,1,3,4,2)),(42,(2,1,3,0,4
 #        if numVotes < worstNumVotes:
 #            worstCandidate, worstNumVotes = candidate, numVotes
 #    return singleTransferableVote(removeCandidate(profile, worstCandidate))
-#
-#
