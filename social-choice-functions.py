4fa67b68450d10c05d02d2008c6459ab7ed72a84
Luke Miles <rashreportloaf@gmail.com>
2016-08-04 10:40:00 -0400

    changed to simple class style and implemented some of the functions in it

--- social-choice-functions.py
+++ social-choice-functions.py
@@ -1,90 +1,103 @@
-# Luke Miles 2016-08-02
-# Writing functions from section 2.2 of the book "Computational Choice"
+import random
 
-#myProfile = {(102,(0,1,2)), (101,(1,2,0)), (100,(2,0,1)), (1,(2,1,0))}
-#myProfile = {(100,(2,1,0)), (100,(0,2,1))}
+profile1 = Profile({0,1,2},{(102,(0,1,2)), (101,(1,2,0)), (100,(2,0,1)), (1,(2,1,0))})
+profile2 = Profile({0,1,2}, {(101,(2,1,0)), (100,(0,2,1))})
+profile3 = Profile({0,1,2},((40,(0,1,2)),(28,(1,2,0)),(32,(2,1,0))))
+profile4=Profile({0,1,2,3,4},{(15,(1,2,4,0,3)),(29,(0,1,3,4,2)),(42,(2,1,3,0,4)),(43,(4,1,3,2,0)),(45,(1,2,3,0,4)),(52,(3,0,1,2,4)),(53,(0,2,1,3,4)),(59,(1,2,3,4,0)),(60,(1,4,3,0,2)),(87,(1,4,0,2,3))}
 
-# 0 is trump, 1 is bernie, 2 is hillary
-#myProfile = {(40,(0,1,2)),(28,(1,2,0)),(32,(2,1,0))}
+class Profile(object):
+    # it would save computation if I made graphs once at the start
+    # todo: combine pairs that have same ballot instead of destroying one
+    def __init__(self, mayors, pairs):
+        self.mayors = mayors
+        self.pairs = pairs
+
+    def netPreference(self, mayor1, mayor2):
+        return sum(numVotes
+                   * sign(ballot.index(mayor2) - ballot.index(mayor1))
+                   for (numVotes, ballot) in self.pairs)
+
+    ## simple scores
+    def copelandScore(self, mayor):
+        return sum(sign(self.netPreference(mayor, mayor2))
+                   for mayor2 in self.mayors)
+    def symmetricBordaScore(self, mayor):
+        return sum(self.netPreference(mayor, mayor2)
+                   for mayor2 in self.mayors)
+
+def makeRandProfile():
+    ranking = range(5)
+    pairs = set()
+    for i in xrange(10):
+        random.shuffle(ranking)
+        pairs.add((random.randint(1,100),tuple(ranking)))
+    return Profile(set(ranking), pairs)
 
 def sign(n):
     return 1 if n > 0 else (-1 if n < 0 else 0)
 
-def netPreference(profile, candidate1, candidate2):
-    return sum(numVoters
-               * sign(ballot.index(candidate2) - ballot.index(candidate1))
-               for (numVoters, ballot) in profile)
-
-## SIMPLE SCORES
-
-def copelandScore(profile, candidate):
-    _, ballot = next(iter(profile))
-    return sum(sign(netPreference(profile, candidate, aCandidate))
-               for aCandidate in ballot)
-
-def symmetricBordaScore(profile, candidate):
-    _, ballot = next(iter(profile))
-    return sum(netPreference(profile, candidate, aCandidate)
-               for aCandidate in ballot)
-
-def asymmetricBordaScore(profile, candidate):
-    _, ballot = next(iter(profile))
-    topScore = len(ballot) - 1
-    return sum(numVoters * (topScore - ballot.index(candidate))
-               for numVoters, ballot in profile)
-
-## OTHER STUFF
-
-def doesParetoDominate(profile, candidate1, candidate2):
-    # assumes all pairs in profile have at least one voter
-    # todo: test if this works
-    return all(ballot.index(candidate1) < ballot.index(candidate2)
-               for _, ballot in profile)
-
-def condorcetWinner(profile):
-    _, ballot = next(iter(profile))
-    for candidate1 in ballot:
-      if any(netPreference(profile, candidate2, candidate1) > 0
-             for candidate2 in ballot):
-          next
-      else:
-          return candidate1
-    return False
-
-def singleTransferableVote(profile):
-    # messy. I think it works.
-    majority = sum(numVoters for numVoters, _ in profile) // 2 + 1
-    _, ballot = next(iter(profile))
-    votersPerCandidate = {candidate: 0 for candidate in ballot}
-    for numVoters, ballot in profile:
-        votersPerCandidate[ballot[0]] += numVoters
-
-    worstCandidate, worstNumVotes = None, float("inf")
-    for candidate, numVoters in votersPerCandidate.items():
-        if numVoters >= majority:
-            return candidate
-        if numVoters < worstNumVotes:
-            worstCandidate, worstNumVotes = candidate, numVoters
-    return singleTransferableVote(removeCandidate(profile, worstCandidate))
-
-def removeCandidate(profile, candidate):
-    # current bug: if two pairs in profile are made identical by the removal
-    # of candidate, then one copy is destroyed
-    return {(numVoters, removeIndex(ballot, ballot.index(candidate)))
-            for (numVoters, ballot) in profile}
-
-def removeIndex(aTuple, index):
-    return aTuple[:index] + aTuple[index+1:]
-
-
-## CONDORCET EXTENSIONS
-
-def simpsonScore(profile, candidate):
-    ballot = set(next(iter(profile))[1]) - {candidate}
-    return min(netPreference(profile, candidate, candidate2)
-               for candidate2 in ballot)
-
-def simpsonSCF(profile):
-    _, ballot = next(iter(profile))
-    return max(ballot, key=lambda candidate: simpsonScore(profile, candidate))
-
+### SIMPLE SCORES
+#
+#
+#
+#def asymmetricBordaScore(profile, candidate):
+#    _, ballot = next(iter(profile))
+#    topScore = len(ballot) - 1
+#    return sum(numVotes * (topScore - ballot.index(candidate))
+#               for numVotes, ballot in profile)
+#
+#
+### CONDORCET EXTENSIONS
+#
+#def simpsonScore(profile, candidate):
+#    ballot = set(next(iter(profile))[1]) - {candidate}
+#    return min(netPreference(profile, candidate, candidate2)
+#               for candidate2 in ballot)
+#
+#def simpsonSCF(profile):
+#    _, ballot = next(iter(profile))
+#    return max(ballot, key=lambda candidate: simpsonScore(profile, candidate))
+#
+#
+### OTHER STUFF
+#
+#def doesParetoDominate(profile, candidate1, candidate2):
+#    # assumes all pairs in profile have at least one voter
+#    # todo: test if this works
+#    return all(ballot.index(candidate1) < ballot.index(candidate2)
+#               for _, ballot in profile)
+#
+#def condorcetWinner(profile):
+#    _, ballot = next(iter(profile))
+#    for candidate1 in ballot:
+#      if any(netPreference(profile, candidate2, candidate1) > 0
+#             for candidate2 in ballot):
+#          next
+#      else:
+#          return candidate1
+#    return False
+#
+#def singleTransferableVote(profile):
+#    # messy. I think it works.
+#    majority = sum(numVotes for numVotes, _ in profile) // 2 + 1
+#    _, ballot = next(iter(profile))
+#    votesPerCandidate = {candidate: 0 for candidate in ballot}
+#    for numVotes, ballot in profile:
+#        votesPerCandidate[ballot[0]] += numVotes
+#
+#    worstCandidate, worstNumVotes = None, float("inf")
+#    for candidate, numVotes in votesPerCandidate.items():
+#        if numVotes >= majority:
+#            return candidate
+#        if numVotes < worstNumVotes:
+#            worstCandidate, worstNumVotes = candidate, numVotes
+#    return singleTransferableVote(removeCandidate(profile, worstCandidate))
+#
+#def removeCandidate(profile, candidate):
+#    # current bug: if two pairs in profile are made identical by the removal
+#    # of candidate, then one copy is destroyed
+#    return {(numVotes, removeIndex(ballot, ballot.index(candidate)))
+#            for (numVotes, ballot) in profile}
+#
+#def removeIndex(aTuple, index):
+#    return aTuple[:index] + aTuple[index+1:]
