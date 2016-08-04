464513be611fbb984d44e778d0f8fdf432863764
Luke Miles <rashreportloaf@gmail.com>
2016-08-04 10:57:05 -0400

    added bordaScore to Profile class. Stable version I think.

--- social-choice-functions.py
+++ social-choice-functions.py
@@ -1,10 +1,5 @@
 import random
 
-profile1 = Profile({0,1,2},{(102,(0,1,2)), (101,(1,2,0)), (100,(2,0,1)), (1,(2,1,0))})
-profile2 = Profile({0,1,2}, {(101,(2,1,0)), (100,(0,2,1))})
-profile3 = Profile({0,1,2},((40,(0,1,2)),(28,(1,2,0)),(32,(2,1,0))))
-profile4=Profile({0,1,2,3,4},{(15,(1,2,4,0,3)),(29,(0,1,3,4,2)),(42,(2,1,3,0,4)),(43,(4,1,3,2,0)),(45,(1,2,3,0,4)),(52,(3,0,1,2,4)),(53,(0,2,1,3,4)),(59,(1,2,3,4,0)),(60,(1,4,3,0,2)),(87,(1,4,0,2,3))}
-
 class Profile(object):
     # it would save computation if I made graphs once at the start
     # todo: combine pairs that have same ballot instead of destroying one
@@ -24,6 +19,10 @@ class Profile(object):
     def symmetricBordaScore(self, mayor):
         return sum(self.netPreference(mayor, mayor2)
                    for mayor2 in self.mayors)
+    def bordaScore(self, mayor):
+        topScore = len(self.mayors) - 1
+        return sum(numVotes * (topScore - ballot.index(mayor))
+                   for numVotes, ballot in self.pairs)
 
 def makeRandProfile():
     ranking = range(5)
@@ -36,15 +35,15 @@ def makeRandProfile():
 def sign(n):
     return 1 if n > 0 else (-1 if n < 0 else 0)
 
+profile1 = Profile({0,1,2},{(102,(0,1,2)), (101,(1,2,0)), (100,(2,0,1)), (1,(2,1,0))})
+profile2 = Profile({0,1,2}, {(101,(2,1,0)), (100,(0,2,1))})
+profile3 = Profile({0,1,2},((40,(0,1,2)),(28,(1,2,0)),(32,(2,1,0))))
+profile4 = Profile({0,1,2,3,4},{(15,(1,2,4,0,3)),(29,(0,1,3,4,2)),(42,(2,1,3,0,4)),(43,(4,1,3,2,0)),(45,(1,2,3,0,4)),(52,(3,0,1,2,4)),(53,(0,2,1,3,4)),(59,(1,2,3,4,0)),(60,(1,4,3,0,2)),(87,(1,4,0,2,3))})
+
 ### SIMPLE SCORES
 #
 #
 #
-#def asymmetricBordaScore(profile, candidate):
-#    _, ballot = next(iter(profile))
-#    topScore = len(ballot) - 1
-#    return sum(numVotes * (topScore - ballot.index(candidate))
-#               for numVotes, ballot in profile)
 #
 #
 ### CONDORCET EXTENSIONS
