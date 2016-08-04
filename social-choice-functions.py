f822f372d937bc4efdabb5ec340a51a9a575ad2e
Luke Miles <rashreportloaf@gmail.com>
2016-08-04 11:05:01 -0400

    added simpsonScore to Profile class. probably stable.

--- social-choice-functions.py
+++ social-choice-functions.py
@@ -24,6 +24,17 @@ class Profile(object):
         return sum(numVotes * (topScore - ballot.index(mayor))
                    for numVotes, ballot in self.pairs)
 
+    ## condorcet consistent scores
+    def simpsonScore(self, mayor):
+        return min(self.netPreference(mayor, mayor2)
+                   for mayor2 in self.mayors - {mayor})
+
+    ## other stuff
+    def removeMayor(self, mayor):
+        pass
+        #return {(numVotes, removeIndex(ballot, ballot.index(candidate)))
+        #        for (numVotes, ballot) in profile}
+
 def makeRandProfile():
     ranking = range(5)
     pairs = set()
@@ -34,24 +45,16 @@ def makeRandProfile():
 
 def sign(n):
     return 1 if n > 0 else (-1 if n < 0 else 0)
+def removeIndex(aTuple, index):
+    return aTuple[:index] + aTuple[index+1:]
 
 profile1 = Profile({0,1,2},{(102,(0,1,2)), (101,(1,2,0)), (100,(2,0,1)), (1,(2,1,0))})
 profile2 = Profile({0,1,2}, {(101,(2,1,0)), (100,(0,2,1))})
 profile3 = Profile({0,1,2},((40,(0,1,2)),(28,(1,2,0)),(32,(2,1,0))))
 profile4 = Profile({0,1,2,3,4},{(15,(1,2,4,0,3)),(29,(0,1,3,4,2)),(42,(2,1,3,0,4)),(43,(4,1,3,2,0)),(45,(1,2,3,0,4)),(52,(3,0,1,2,4)),(53,(0,2,1,3,4)),(59,(1,2,3,4,0)),(60,(1,4,3,0,2)),(87,(1,4,0,2,3))})
 
-### SIMPLE SCORES
-#
-#
-#
-#
-#
 ### CONDORCET EXTENSIONS
 #
-#def simpsonScore(profile, candidate):
-#    ballot = set(next(iter(profile))[1]) - {candidate}
-#    return min(netPreference(profile, candidate, candidate2)
-#               for candidate2 in ballot)
 #
 #def simpsonSCF(profile):
 #    _, ballot = next(iter(profile))
@@ -92,11 +95,4 @@ profile4 = Profile({0,1,2,3,4},{(15,(1,2,4,0,3)),(29,(0,1,3,4,2)),(42,(2,1,3,0,4
 #            worstCandidate, worstNumVotes = candidate, numVotes
 #    return singleTransferableVote(removeCandidate(profile, worstCandidate))
 #
-#def removeCandidate(profile, candidate):
-#    # current bug: if two pairs in profile are made identical by the removal
-#    # of candidate, then one copy is destroyed
-#    return {(numVotes, removeIndex(ballot, ballot.index(candidate)))
-#            for (numVotes, ballot) in profile}
 #
-#def removeIndex(aTuple, index):
-#    return aTuple[:index] + aTuple[index+1:]
