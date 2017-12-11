575df9d639d430e0c3321cb5d76510d7871faa93
Bernardo Trevizan <trevizanbernardo@gmail.com>
2017-12-11 15:38:40 -0200

    New version (1.2)

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
 	name = 'social_choice',
 	packages = ['social_choice'],
-	version = '1.1',
+	version = '1.2',
 	description = 'Social choice functions implemented in Python 3.',
 	author = 'Bernardo Trevizan',
 	author_email = 'trevizanbernardo@gmail.com',
