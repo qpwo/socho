576b319b657309dd4651b06577316c0aea4c8365
Bernardo Trevizan <trevizanbernardo@gmail.com>
2017-12-15 12:45:58 -0200

    New subversion

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
 	name = 'social_choice',
 	packages = ['social_choice'],
-	version = '1.2',
+	version = '1.2.1',
 	description = 'Social choice functions implemented in Python 3.',
 	author = 'Bernardo Trevizan',
 	author_email = 'trevizanbernardo@gmail.com',
