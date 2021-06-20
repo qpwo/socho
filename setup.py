3041d11447786bb10dbe6728cc6627a3251f9d3e
Luke Harold Miles <luke@cs.uky.edu>
2021-06-20 16:26:08 -0400

    add readme to setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 from distutils.core import setup
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
 	name = 'socho',
 	packages = ['socho'],
-	version = '1.3.1',
+	version = '1.3.2',
 	scripts = ['bin/socho.py'],
-	description = 'Social choice functions implemented in Python 3.',
+	description = 'Social choice functions and CLI tool',
+	long_description = long_description,
+    long_description_content_type = "text/markdown",
 	author = 'Luke Harold Miles, Bernardo Trevizan',
 	author_email = 'lukem@sent.com, trevizanbernardo@gmail.com',
 	url = 'https://github.com/qpwo/socho'
