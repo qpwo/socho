24feb730956e92713cc20f97663ee9933733e364
Luke Harold Miles <luke@cs.uky.edu>
2021-06-20 16:51:05 -0400

    separate requirements and setup again

--- setup.py
+++ setup.py
@@ -3,15 +3,12 @@ from distutils.core import setup
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-with open('requirements.txt') as f:
-    required = f.read().splitlines()
-
 setup(
 	name = 'socho',
 	packages = ['socho'],
 	version = '1.3.3',
 	description = 'Social choice functions and CLI tool',
-	install_requires = required,
+	install_requires = ['numpy', 'pandas'],
 	long_description = long_description,
     long_description_content_type = "text/markdown",
 	author = 'Luke Harold Miles, Bernardo Trevizan',
